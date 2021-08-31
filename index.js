c98b8ff7eac183fb0ee76fc9f590b30bd444f6e2
nikki-shahh <84827772+nikki-shahh@users.noreply.github.com>
2021-08-30 20:13:35 -0700

    updates

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const express = require('express'),
     bodyParser = require('body-parser'),
     uuid = require ('uuid')
 
+const { check, validationResult } = require('express-validator');
+
 const mongoose = require('mongoose');
 const Models = require('./models/models.js');
 
@@ -19,17 +21,14 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const cors = require('cors');
-  app.use(cors());
 
 let auth = require('./auth.js')(app);
 const passport = require('passport');
 require('./passport.js');
 
-const { check, validationResult } = require('express-validator');
-
 app.use(express.static('public'));
 app.use(morgan('common'));
-
+app.use(cors());
 
 
 app.get('/', (req, res)=>{
@@ -85,16 +84,16 @@ app.get ('/directors/:Name', passport.authenticate('jwt',{session: false}) ,(req
 
 //Return all users
 
-app.get('/users' , passport.authenticate('jwt',{session: false}),(req, res) => {
-    Users.find()
-      .then((users) => {
-        res.status(201).json(users);
-      })
-      .catch((err) => {
-        console.error(err);
-        res.status(500).send('Error: ' + err);
-      });
-  });
+//app.get('/users' , passport.authenticate('jwt',{session: false}),(req, res) => {
+    //Users.find()
+      //.then((users) => {
+       // res.status(201).json(users);
+      //})
+      //.catch((err) => {
+       // console.error(err);
+        //res.status(500).send('Error: ' + err);
+     // });
+  //});
 
 //Allow new users to register
 
@@ -135,23 +134,24 @@ app.post('/users' ,
   });
 
 //Allow users to update their user info
-app.put('/users/:Username', passport.authenticate('jwt',{session: false}) ,
+app.put('/users/:Username' ,
 [
   check('Username', 'Username is required').isLength({min: 5}),
   check('Username', 'Username contains non alphanumeric characters - not allowed.').isAlphanumeric(),
   check('Password', 'Password is required').not().isEmpty(),
   check('Email', 'Email does not appear to be valid').isEmail()
 ]
-, (req, res) => {
+,passport.authenticate('jwt',{session: false}) ,(req, res) => {
   let errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(422).json({ errors: errors.array() });
   }
+  let hashedPassword = Users.hashPassword(req.body.Password);
   Users.findOneAndUpdate({ Username: req.params.Username }, 
     { 
       $set:{
       Username: req.body.Username,
-      Password: req.body.Password,
+      Password: hashedPassword,
       Email: req.body.Email,
       Birthday: req.body.Birthday
     }
